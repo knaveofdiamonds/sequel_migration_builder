434632f65bd711b6daa6089e1105e206d90af325
Roland Swingler <roland.swingler@gmail.com>
2010-06-11 14:16:08 +0100

    MigrationBuilder refactoring and primary key support.

--- lib/sequel/migration_builder.rb
+++ lib/sequel/migration_builder.rb
@@ -8,12 +8,14 @@ module Sequel
 
     def initialize(db)
       @db = db
+      @db_tables = Schema::DbSchemaParser.for_db(db).parse_db_schema
+      @db_table_names = @db.tables
       @indent = 0
       @result = []
     end
 
     def generate_migration(tables)
-      return if tables.empty?
+      return if tables.empty? && @db_tables.empty?
       result.clear
 
       add_line "Sequel.migration do"
@@ -26,41 +28,53 @@ module Sequel
       result.join("\n")
     end
 
-    private
-
     def generate_up(tables)
-      i = 0 
+      current_tables, new_tables = table_names(tables).partition do |table_name| 
+        @db_table_names.include?(table_name)
+      end
 
       add_line "up do"
-      table_names(tables).each do |table_name|
-        i += 1
-        table = tables[table_name]
-        indent do
-          unless @db.table_exists?(table_name)
-            add_line "create_table #{table_name.inspect} do"
-            indent do
-              table[:columns].each do |column|
-                add_line Schema::DbColumn.build_from_hash(column).define_statement
-              end
-            end
-            add_line "end"
-          end
-          add_blank_line unless i == tables.size
-        end
-      end        
+      create_new_tables(new_tables, tables)
       add_line "end"
       add_blank_line
     end
 
     def generate_down(tables)
+      current_tables, new_tables = table_names(tables).partition do |table_name| 
+        @db_table_names.include?(table_name)
+      end
+
       add_line "down do"
       indent do
-        table_names(tables).reverse.each do |table_name| 
-          add_line "drop_table #{table_name.inspect}" unless @db.table_exists?(table_name)
+        new_tables.reverse.each {|table_name| add_line "drop_table #{table_name.inspect}" }
+      end
+      add_line "end"
+    end
+
+    def create_new_tables(new_tables, tables)
+      i = 0
+      new_tables.each do |table_name|
+        i += 1
+        indent { create_table_statement table_name, tables[table_name] }
+        add_blank_line unless i == tables.size
+      end
+    end
+
+    def create_table_statement(table_name, table)
+      add_line "create_table #{table_name.inspect} do"
+      indent do
+        table[:columns].map {|c| Schema::DbColumn.build_from_hash(c) }.each do |column|
+          add_line column.define_statement
+        end
+        if table[:primary_key]
+          add_blank_line
+          add_line "primary_key #{table[:primary_key].inspect}"
         end
       end
       add_line "end"
     end
+
+    private
 
     attr_reader :result
 
--- spec/migration_builder_spec.rb
+++ spec/migration_builder_spec.rb
@@ -2,8 +2,10 @@ require File.dirname(__FILE__) + "/spec_helper"
 
 describe Sequel::MigrationBuilder do
   
-  it "should return nil if the table hash is empty" do
-    Sequel::MigrationBuilder.new(stub(:db)).generate_migration({}).should be_nil
+  it "should return nil if the table hash is empty and the database has no tables" do
+    mock_db = mock(:database)
+    mock_db.should_receive(:tables).at_least(:once).and_return([])
+    Sequel::MigrationBuilder.new(mock_db).generate_migration({}).should be_nil
   end
 
   it "should produce a simple migration string given a database connection and a hash of tables" do
@@ -27,7 +29,7 @@ end
 END
     
     mock_db = mock(:database)
-    mock_db.should_receive(:table_exists?).at_least(:once).and_return(false)
+    mock_db.should_receive(:tables).at_least(:once).and_return([])
     Sequel::MigrationBuilder.new(mock_db).generate_migration(tables).should == expected
   end
 
@@ -62,7 +64,42 @@ end
 END
     
     mock_db = mock(:database)
-    mock_db.should_receive(:table_exists?).at_least(:once).and_return(false)
+    mock_db.should_receive(:tables).at_least(:once).and_return([])
     Sequel::MigrationBuilder.new(mock_db).generate_migration(tables).should == expected
+  end
+
+  it "should add the primary key of the table" do
+    mock_db = mock(:database)
+    mock_db.should_receive(:tables).at_least(:once).and_return([])
+    table = {
+      :primary_key => :foo,
+      :columns => [{:name => :foo, :column_type => :integer}, {:name => :bar, :column_type => :varchar}]
+    }
+
+    expected = <<-END
+create_table :example_table do
+  integer :foo, :null => false
+  varchar :bar, :null => false
+
+  primary_key :foo
+end
+END
+
+    Sequel::MigrationBuilder.new(mock_db).create_table_statement(:example_table, table).join("\n").
+      should == expected.strip
+  end
+
+
+  it "should drop the table if the table exists in the database but not the table hash" do
+    pending # Deal with in a later version.
+    mock_db = mock(:database)
+    mock_db.should_receive(:tables).at_least(:once).and_return([:example_table])
+    
+    expected = <<-END
+up do
+  drop_table :example_table
+end
+END
+    Sequel::MigrationBuilder.new(mock_db).generate_up({}).join("\n").should == expected
   end
 end
