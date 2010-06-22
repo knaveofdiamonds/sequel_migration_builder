00af3f2c9706d7b1c5adb4abd92afa53c5b93e44
Roland Swingler <roland.swingler@gmail.com>
2010-06-22 15:20:56 +0100

    Requiring sequel in spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+require 'rubygems'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'sequel'
 require 'sequel/migration_builder'
 require 'spec'
 require 'spec/autorun'
