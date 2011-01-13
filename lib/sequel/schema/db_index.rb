dec2b2d0d7c4da81e2e69c49958a0a2dc2122d85
Roland Swingler <roland.swingler@gmail.com>
2011-01-13 10:49:01 +0000

    Documenting DbIndex.

--- lib/sequel/schema/db_index.rb
+++ lib/sequel/schema/db_index.rb
@@ -4,34 +4,48 @@ module Sequel
       attr_accessor :name
       attr_reader :columns
 
-      # Builds an Array of indexes from the index hash returned by
+      # Builds an Array of DbIndexes from the index hash returned by
       # Sequel::Database#indexes
       def self.build_from_hash(definitions)
         definitions.map {|name,attrs| new(name, attrs[:columns], attrs[:unique]) }
       end
-      
+
+      # Creates a new DbIndex definition.
+      #
+      # columns may be a single column name as a symbol, or an array
+      # of column symbol names.
+      #
+      # Indexes are not unique by default.
       def initialize(name, columns, unique=false)
         @name = name.to_sym
         @columns = columns.kind_of?(Array) ? columns.clone : [columns]
         @unique = unique
       end
 
+      # Returns true if this index is unique
       def unique?
         !! @unique
       end
 
+      # Returns true if this index has more than one column.
       def multi_column?
         @columns.size > 1
       end
-      
+
+      # Returns the sequel migration statement to define an index in a
+      # create_table block.
       def define_statement
         base_add_statement('index')
       end
 
+      # Returns the sequel migration statement to add an index in an
+      # alter_table block.
       def add_statement
         base_add_statement('add_index')
       end
 
+      # Returns the sequel migration statement to remove an index in an
+      # alter_table block.
       def drop_statement
         "drop_index #{columns_for_statement.inspect}, :name => #{name.inspect}"
       end
@@ -48,7 +62,7 @@ module Sequel
       end
       alias :eql? :==
       
-      def hash
+      def hash # :nodoc:
         @name.hash
       end
 
